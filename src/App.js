2ba4eec3b6b986bd79d58c0bba5ef376916ad4d5
CristhianDaza <cristian.d.b@hotmail.com>
2020-04-02 22:14:37 -0500

    Creando componente tarjeta con sus props

--- src/App.js
+++ src/App.js
@@ -18,12 +18,29 @@ const App = () => (
     </div>
 
     <div className="ed-grid m-grid-3">
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
+      <Curso 
+        titulo="React desde Cero" 
+        imagenCurso="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png" 
+        precio="75.000 pesos"
+        profesor="Cristhian Daza"
+        imagenProfesor="https://scontent-bog1-1.xx.fbcdn.net/v/t1.0-0/p480x480/69584194_10157856948884684_1057082779730182144_o.jpg?_nc_cat=108&_nc_sid=7aed08&_nc_eui2=AeEjt6EkDqXSYyCn5j1Acx-F6Linn_IYpx_z_CjWFJQeP5emydbo2e6nUrqPzkzfxSERz3l5-d1eSj27hkshNFdnxq9dFGmI-d8awnIMH5qY1g&_nc_ohc=fW1ypQYRGIEAX8Fb_ho&_nc_ht=scontent-bog1-1.xx&_nc_tp=6&oh=759292a3547e17910009fd23be7378d5&oe=5EAAAB43"
+      />
+
+      <Curso 
+        titulo="React - Manejo del estado de la aplicación" 
+        imagenCurso="https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20Rutas%20manejo%20de%20estados%20%281%29.png" 
+        precio="100.000 pesos"
+        profesor="Cristhian Daza"
+        imagenProfesor="https://scontent-bog1-1.xx.fbcdn.net/v/t1.0-0/p480x480/69584194_10157856948884684_1057082779730182144_o.jpg?_nc_cat=108&_nc_sid=7aed08&_nc_eui2=AeEjt6EkDqXSYyCn5j1Acx-F6Linn_IYpx_z_CjWFJQeP5emydbo2e6nUrqPzkzfxSERz3l5-d1eSj27hkshNFdnxq9dFGmI-d8awnIMH5qY1g&_nc_ohc=fW1ypQYRGIEAX8Fb_ho&_nc_ht=scontent-bog1-1.xx&_nc_tp=6&oh=759292a3547e17910009fd23be7378d5&oe=5EAAAB43"
+      />
+
+      <Curso 
+        titulo="Plataforma de educación online con React.js" 
+        imagenCurso="https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20plataforma%20de%20cursos.png" 
+        precio="350.000 pesos"
+        profesor="Cristhian Daza"
+        imagenProfesor="https://scontent-bog1-1.xx.fbcdn.net/v/t1.0-0/p480x480/69584194_10157856948884684_1057082779730182144_o.jpg?_nc_cat=108&_nc_sid=7aed08&_nc_eui2=AeEjt6EkDqXSYyCn5j1Acx-F6Linn_IYpx_z_CjWFJQeP5emydbo2e6nUrqPzkzfxSERz3l5-d1eSj27hkshNFdnxq9dFGmI-d8awnIMH5qY1g&_nc_ohc=fW1ypQYRGIEAX8Fb_ho&_nc_ht=scontent-bog1-1.xx&_nc_tp=6&oh=759292a3547e17910009fd23be7378d5&oe=5EAAAB43"
+      />
     </div>
   </>
 );
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
-const Curso = () => (
+const Curso = (props) => (
   <article className="card">
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img src="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png" alt="Curso" />
+      <img src={props.imagenCurso} alt="Curso" />
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
       <h3 className="t5 s-mb-2 s-center">
-        Curso de React
+       {props.titulo}
       </h3>
       <div className="s-mb-2 s-main-center">
         <div className="card__teacher s-cross-center">
           <div className="card__avatar s-mr-1">
             <div className="circle img-container">
-              <img src="https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-03/paula-leon_400x400.png" alt="Profesora" />
+              <img src={props.imagenProfesor} alt={props.titulo} />
             </div>
           </div>
-          <span className="small">Cristhian Daza</span>
+          <span className="small">{props.profesor}</span>
         </div>
       </div>
       <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="/">$ 20USD</a>
+        <a className="button--ghost-alert button--tiny" href="/">$ {props.precio}</a>
       </div>
     </div>
   </article>
